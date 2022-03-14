76a027985bdc02ae548e8a8d176e150ca7921847
Max Paulo <maxpauloteixeira@gmail.com>
2022-03-14 19:06:54 -0300

    Melhorando a visualização das receitas no Admin.

--- receitas/admin.py
+++ receitas/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Receitas
 # Register your models here.
 
-admin.site.register(Receitas)
+class DisplayReceitas(admin.ModelAdmin):
+    list_display = ('id', 'nome_receita')
+    list_display_links = ('id', 'nome_receita')
+
+admin.site.register(Receitas, DisplayReceitas)
