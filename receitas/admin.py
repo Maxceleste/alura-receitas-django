48049e44e64c9ebdc2b38e5ae79f1dcfa75db5b3
Max Paulo <maxpauloteixeira@gmail.com>
2022-03-16 16:16:13 -0300

    Melhorando o Django adm

--- receitas/admin.py
+++ receitas/admin.py
@@ -3,7 +3,10 @@ from .models import Receitas
 # Register your models here.
 
 class DisplayReceitas(admin.ModelAdmin):
-    list_display = ('id', 'nome_receita')
+    list_display = ('id', 'nome_receita', 'categoria')
     list_display_links = ('id', 'nome_receita')
+    search_fields = ('nome_receita',)
+    list_filter = ('categoria',)
+    list_per_page = 10
 
 admin.site.register(Receitas, DisplayReceitas)
