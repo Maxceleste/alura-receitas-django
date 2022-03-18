539d9ed6bc478f18c838d7874abd893769563ed6
Max Paulo <maxpauloteixeira@gmail.com>
2022-03-18 17:13:32 -0300

    Colocando a opçao de editar se esta publicado mais facil

--- receitas/admin.py
+++ receitas/admin.py
@@ -3,10 +3,11 @@ from .models import Receitas
 # Register your models here.
 
 class DisplayReceitas(admin.ModelAdmin):
-    list_display = ('id', 'nome_receita', 'categoria')
+    list_display = ('id', 'nome_receita', 'categoria', 'publicado')
     list_display_links = ('id', 'nome_receita')
     search_fields = ('nome_receita',)
     list_filter = ('categoria',)
+    list_editable = ('publicado',)
     list_per_page = 10
 
 admin.site.register(Receitas, DisplayReceitas)
