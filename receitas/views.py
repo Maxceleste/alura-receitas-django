3f0cc240b1bcfa009aee2c428d3cd4128d736a39
Maxceleste <maxpauloteixeira@gmail.com>
2022-04-09 17:26:41 -0300

    Adicionando a possibilidade de buscar uma receita

--- receitas/views.py
+++ receitas/views.py
@@ -22,4 +22,17 @@ def receita(request, receita_id):
 
 
 def buscar(request):
-    return render(request, 'buscar.html')
+
+    lista_receitas = Receitas.objects.order_by('-date_receita').filter(publicado = True)
+
+    if 'busca' in request.GET:
+        nome_a_buscar = request.GET['busca']
+        if nome_a_buscar:
+            lista_receitas = lista_receitas.filter(nome_receita__icontains=nome_a_buscar)
+        
+    dados = {
+        'receitas' : lista_receitas
+    }
+
+
+    return render(request, 'buscar.html', dados)
