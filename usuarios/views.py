8a4e319bd907e55b7ad3bd9b22d2303bb6f2cd31
Maxceleste <maxpauloteixeira@gmail.com>
2022-05-04 17:41:22 -0300

    Finalizando o cadastro de usuários

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 def cadastro(request):
     if request.method == 'POST':
-        print('Usuário cadastrado!')
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['password']
+        senha2 = request.POST['password2']
+
+        if not nome.strip():
+            print('O campo nome não pode ficar em branco')
+            return redirect('cadastro')
+        
+        if not email.strip():
+            print('O campo email não pode ficar em branco')
+            return redirect('cadastro')
+
+        if senha != senha2:
+            print('As senhas estão diferentes.')
+            return redirect('cadastro')
+
+        if User.objects.filter(email=email).exists():
+            print('Usuário já existe')
+            return redirect('cadastro')
+        
+        user = User.objects.create_user(username = nome, email = email, password = senha)
+        user.save()
+
+        print('Usuário cadastrado')
         return redirect('login')
     else:
         return render(request, 'usuarios/cadastro.html')
