5c80fd1aac60e2f369cf1b2fc63cabe0c5cf020a
Maxceleste <maxpauloteixeira@gmail.com>
2022-05-07 18:02:04 -0300

    Adicionando a possibilidade de login e dashboard

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib import auth
 
 def cadastro(request):
     if request.method == 'POST':
@@ -32,11 +33,30 @@ def cadastro(request):
     else:
         return render(request, 'usuarios/cadastro.html')
 
+
+
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        senha = request.POST['senha']
+
+        if email == '' or senha == '':
+            print('Prencha todos os campos.')
+            return redirect('login')
+
+        if User.objects.filter(email=email).exists():
+            nome = User.objects.filter(email=email).values_list('username', flat = True)[0]
+            user = auth.authenticate(request, username = nome, password = senha)
+            if user is not None:
+                auth.login(request, user)
+                print('Login realizado com sucesso!')
+                return redirect('dashboard')
+
     return render(request, 'usuarios/login.html')
 
+
 def dashboard(request):
-    pass
+    return render(request, 'usuarios/dashboard.html')
 
 def logout(request):
     pass
